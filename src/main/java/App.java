36acab641fafb0cf15f0f4a2c4306882d666817e
Kate Mashokha <katyamashokha@yahoo.com>
2017-08-11 09:03:48 -0700

    Setup an initial project

---
+++ src/main/java/App.java
@@ -0,0 +1,5 @@
+/**
+ * Created by katsiarynamashokha on 8/11/17.
+ */
+public class App {
+}
---
+++ src/main/java/models/Member.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by katsiarynamashokha on 8/11/17.
+ */
+public class Member {
+}
