3c0b63299dfa3d27c43537a0d992cf590bf07742
Kate Mashokha <katyamashokha@yahoo.com>
2017-08-18 10:23:18 -0700

    Add a test that makes sure that a team member can be added

--- src/test/java/dao/MemberDaoTest.java
+++ src/test/java/dao/MemberDaoTest.java
@@ -1,8 +1,11 @@
 package dao;
 
+import models.Member;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
@@ -10,19 +13,33 @@ import static org.junit.Assert.*;
  * Created by katsiarynamashokha on 8/18/17.
  */
 public class MemberDaoTest {
+    private Sql2oMemberDao memberDao;
+    private Connection con;
+
     @Before
     public void setUp() throws Exception {
+        final String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        memberDao = new Sql2oMemberDao(sql2o);
+        con = sql2o.open();
+    }
 
+    // helper to create a new team member
+    private Member createMember() {
+        return new Member("Alice", 19, 34);
     }
 
     @After
     public void tearDown() throws Exception {
-
+        con.close();
     }
 
     @Test
     public void add() throws Exception {
-
+        Member testMember  = createMember();
+        memberDao.add(testMember);
+        assertEquals(1, testMember.getMemberId());
+        assertEquals(34, testMember.getTeamId());
     }
 
     @Test
